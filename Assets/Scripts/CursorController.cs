13ac5b185ec4893346d21bbb386cc340a1be86a3
George Brown <396gbrown@gmail.com>
2019-10-10 19:46:10 -0400

    Added Player Movement, Projectile Logic, and Corresponding UI Elements (#3)
    * Added Projectile Shooting Functionality and Corresponding UI elements
    
    * Added Player Controller Logic and Corresponding UI elements

---
+++ Assets/Scripts/CursorController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorController : MonoBehaviour
+{
+
+
+    public Texture2D inPlayCursor;
+    public Texture2D menuCursor;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+
+    void Start()
+    {
+        Cursor.SetCursor(inPlayCursor, hotSpot, cursorMode);
+    }
+
+    void OnMouseEnter()
+    {
+       if (gameObject.tag == "UI") {
+            Cursor.SetCursor(menuCursor, hotSpot, cursorMode);
+       }
+    }
+
+    void OnMouseExit()
+    {
+        Cursor.SetCursor(inPlayCursor, hotSpot, cursorMode);
+    }
+
+}
